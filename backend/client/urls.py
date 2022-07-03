9d255ac8eab5d2d64f7fcb36cb88a67e9cdbfb19
RafsanRifat <rafsan.rifat255@gmail.com>
2022-07-03 11:13:45 +0600

    post method added for all items

--- backend/client/urls.py
+++ backend/client/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import client_list, item_list, invoice_list
+from .views import client, item_list, invoice_list
 
 
 urlpatterns = [
-    path('api/clients', client_list, name="client_list"),
+    path('api/clients', client, name="client"),
     path('api/items', item_list, name="items"),
     path('api/invoices', invoice_list, name="invoices")
 ]
--- backend/client/views.py
+++ backend/client/views.py
@@ -7,22 +7,40 @@ from rest_framework.decorators import api_view
 
 # Create your views here.
 
-@api_view(['GET'])
-def client_list(request):
+@api_view(['GET', 'POST'])
+def client(request):
+    if request.method == 'POST':
+        serilizer = Clientsserializer(data=request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+        else:
+            return Response({"Message": "Something went wrong"})
     clients = Clients.objects.all()
     clients = Clientsserializer(clients, many=True)
     return Response(clients.data)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def item_list(request):
+    if request.method == 'POST':
+        serilizer = ItemsSerializer(data=request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+        else:
+            return Response({"Message": "Something went wrong"})
     items = Items.objects.all()
     items = ItemsSerializer(items, many=True)
     return Response(items.data)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def invoice_list(request):
+    if request.method == 'POST':
+        serilizer = InvoiceSerilizer(data=request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+        else:
+            return Response({"Message": "Something went wrong"})
     invoices = Invoices.objects.all()
     invoices = InvoiceSerilizer(invoices, many=True)
     return Response(invoices.data)
